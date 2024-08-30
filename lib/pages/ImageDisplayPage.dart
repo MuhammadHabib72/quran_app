88894073594e920da746d162b67dd2a89da648a2
unknown <mrbones03339@gmail.com>
2024-08-30 16:09:58 +0500

    new changes by habib

--- lib/pages/ImageDisplayPage.dart
+++ lib/pages/ImageDisplayPage.dart
@@ -11,8 +11,13 @@ class ImageDisplayPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.white),
         title: Text(title),
-        backgroundColor: const Color.fromARGB(255, 54, 8, 99),
+        titleTextStyle: const TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+        ),
+        backgroundColor: const Color.fromARGB(255, 92, 44, 141),
       ),
       backgroundColor:
           const Color.fromARGB(255, 232, 201, 240), // Background color
--- lib/pages/starting_page.dart
+++ lib/pages/starting_page.dart
@@ -18,7 +18,7 @@ class StartingPage extends StatelessWidget {
             const Text(
               'Qari. Ai',
               style: TextStyle(
-                fontSize: 30,
+                fontSize: 35,
                 fontWeight: FontWeight.w900,
                 color: Color.fromRGBO(48, 7, 89, 1),
               ),
@@ -28,92 +28,82 @@ class StartingPage extends StatelessWidget {
               'Learn , Memorize and Recite\nQuran Anywhere Anytime',
               textAlign: TextAlign.center,
               style: TextStyle(
-                fontSize: 16,
+                fontSize: 22,
                 fontWeight: FontWeight.w700,
                 color: Color.fromRGBO(157, 29, 242, 1),
               ),
             ),
             const SizedBox(height: 20),
             // Center section with image and "Welcome" text
-            Stack(
-                alignment: Alignment.bottomCenter,
-                clipBehavior: Clip.none, // Allow overflow for button
+            Container(
+              width: MediaQuery.of(context).size.width *
+                  0.9, // 90% of screen width
+              padding: const EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(
+                    255, 54, 8, 99), // Purple background color
+                borderRadius: BorderRadius.circular(20), // Rounded corners
+              ),
+              child: Column(
                 children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width *
-                        0.9, // 90% of screen width
-                    padding: const EdgeInsets.all(20.0),
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(
-                          255, 54, 8, 99), // Purple background color
-                      borderRadius:
-                          BorderRadius.circular(20), // Rounded corners
-                    ),
-                    child: Column(
-                      children: [
-                        const Text(
-                          'Welcome',
-                          style: TextStyle(
-                            fontSize: 40,
-                            fontWeight: FontWeight.w900,
-                            color: Colors.white,
-                          ),
-                        ),
-                        // Spacing between text and image
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 20.0),
-                          child: Container(
-                            height: 360, // Adjusted height to match the design
-                            child: ColorFiltered(
-                              colorFilter: const ColorFilter.mode(
-                                Colors.transparent, // Adjust the filter color
-                                BlendMode.dstOut, // Blend mode to remove black
-                              ),
-                              child: Image.asset(
-                                'assets/Masjid.png',
-                                fit: BoxFit.contain,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
+                  const Text(
+                    'Welcome',
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.white,
                     ),
                   ),
-                  // Get Started button
-                  Positioned(
-                    bottom: -35, // Adjust this value to control the overlap
-                    child: GestureDetector(
-                      onTap: () => Navigator.pushNamed(context, '/Inner'),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Color.fromRGBO(157, 29, 242, 1),
-                          borderRadius: BorderRadius.circular(50),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.2),
-
-                              blurRadius: 10,
-                              offset:
-                                  Offset(0, 5), // changes position of shadow
-                            ),
-                          ],
+                  // Spacing between text and image
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20.0),
+                    child: Container(
+                      height: 300, // Adjusted height to match the design
+                      child: ColorFiltered(
+                        colorFilter: const ColorFilter.mode(
+                          Colors.transparent, // Adjust the filter color
+                          BlendMode.dstOut, // Blend mode to remove black
                         ),
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 50,
-                          vertical: 11,
-                        ),
-                        child: const Text(
-                          'Get Started',
-                          style: TextStyle(
-                            fontSize: 31,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.white,
-                          ),
+                        child: Image.asset(
+                          'assets/Masjid.png',
+                          fit: BoxFit.contain,
                         ),
                       ),
                     ),
                   ),
-                ]),
+                ],
+              ),
+            ),
+            const SizedBox(height: 10), // Add spacing between image and button
+            // Get Started button
+            GestureDetector(
+              onTap: () => Navigator.pushNamed(context, '/Inner'),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(157, 29, 242, 1),
+                  borderRadius: BorderRadius.circular(50),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      blurRadius: 10,
+                      offset: const Offset(0, 5), // changes position of shadow
+                    ),
+                  ],
+                ),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 50,
+                  vertical: 12,
+                ),
+                child: const Text(
+                  'Get Started',
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.w300,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
